88f7a1ea2ec207a362ba3b16874cf5d73ae0e230
접근금지 <HAEUN@appleui-iMac.skbroadband>
2024-03-22 10:29:00 +0900

    finish project

--- js/custom.js
+++ js/custom.js
@@ -186,21 +186,22 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   workAnimation();
+  const pcDataSpeed = ['500', '400', '800', '600'];
+  const mobileDataSpeed = ['150', '180', '250', '180'];
 
-  function serviceAnimation() {
+  function serviceAnimation(speed) {
     const arrow_elmts = gsap.utils.toArray('.service-arrow');
 
-    arrow_elmts.forEach((num) => {
-      const data_speed = num.getAttribute('data-speed');
+    arrow_elmts.forEach((num, i) => {
+      console.log(speed[i]);
+      // const data_speed = num.getAttribute('data-speed');
 
       tl.from(num, {
         scrollTrigger: commonScrollTrigger.service,
-        x: -data_speed,
+        x: -speed[i],
       });
     });
   }
-
-  serviceAnimation();
 
   function footerAnimation() {
     const letter_elmts = gsap.utils.toArray('.footer-wrapper span');
@@ -221,8 +222,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
   if (wWidth > 1300) {
     headerAnimation(-70);
+    serviceAnimation(pcDataSpeed);
   } else {
     headerAnimation(0);
+    serviceAnimation(mobileDataSpeed);
   }
 
   //==================================
